75a2fb7e61fb5769b41431d1fbcdddc7240aeeb1
Saliha Apak <salihaapak@gmail.com>
2023-02-05 17:47:14 +0300

    first commit

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using PasswordGenerator;
+using System;
+
+
+RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
+
+
+Console.WriteLine("\n ****************** PASSWORD GENERATOR ****************** \n");
+
+Console.WriteLine("\nDoes your password contain special characters? (Y/N)");
+string SPECIALCHARACTER = Console.ReadLine();
+
+Console.WriteLine("\nDoes your password contain numbers? (Y/N)");
+string NUMBERS = Console.ReadLine();
+
+Console.WriteLine("\nDoes your password contain lower letters? (Y/N)");
+string LOWERCASE = Console.ReadLine();
+
+
+Console.WriteLine("\nDoes your password contain UPPER letter? (Y/N)");
+string UPPERCASE = Console.ReadLine();
+
+
+Console.WriteLine("\nHow many characters should your password consist of?");
+int Lengthofpassword = Convert.ToInt32(Console.ReadLine());
+
+
+string result;
+result = randomPasswordGenerator.RandomPassword(SPECIALCHARACTER, NUMBERS, LOWERCASE, UPPERCASE, Lengthofpassword);
+
+Console.WriteLine($"\nYour Random Password:\n{result}");
+Console.ReadKey();
---
+++ PasswordGenerator/RandomPasswordGenerator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    public class RandomPasswordGenerator
+    {
+
+        const string SpecialCharacter = @"#()$%^-*=&!_+<>";
+        const string Numbers = "0123456789";
+        const string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+        const string Uppercase = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+                 
+        public string RandomPassword(string SPECIALCHARACTER, string NUMBERS, string LOWERCASE, string UPPERCASE, int Lengthofpassword)
+        {
+            char[] password = new char[Lengthofpassword];
+            string RPassword = "";
+            System.Random random = new Random();
+
+            switch (SPECIALCHARACTER)
+            {
+                case "Y" or "y":
+                    RPassword += SpecialCharacter;
+                    break;
+                case "N" or "n":
+                    RPassword += RPassword;
+                    break;
+                    
+            }
+
+
+            switch (NUMBERS)
+            {
+                case "Y" or "y":
+                    RPassword += Numbers;
+                    break;
+                case "N" or "n":
+                    RPassword += RPassword;
+                    break;
+            }
+
+            switch (LOWERCASE)
+            {
+                case "Y" or "y":
+                    RPassword += Lowercase;
+                    break;
+                case "N" or "n":
+                    RPassword += RPassword;
+                    break;
+            }
+
+            switch (UPPERCASE)
+            {
+                case "Y" or "y":
+                    RPassword += Uppercase;
+                    break;
+                case "N" or "n":
+                    RPassword += RPassword;
+                    break;
+            }
+
+            if (RPassword.Length == 0)
+            {
+                return "Password creation failed!";
+            }
+
+
+            for (int i = 0; i < Lengthofpassword; i++)
+                {
+                    password[i] = RPassword[random.Next(RPassword.Length - 1)];
+                }
+            
+            return string.Join(null, password);
+
+        }
+    }
+} 
